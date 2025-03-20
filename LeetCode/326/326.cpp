bee1b9eeb68fcedac9190664e1f3f4ea02c76e4a
BETA_TOT🌸🌸β <lixinzhouli123@gmail.com>
2025-03-20 21:40:16 +0800

    Update 326.cpp

--- LeetCode/326/326.cpp
+++ LeetCode/326/326.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 class Solution {
 public:
-    bool isPowerOfThree(int n) {  //使用递归，3的n次幂特点：1.是3倍数；2.不断/3，最后一定是3/3=1；
-        if (n == 0)return false;  //上一次n未够3
-        if (n == 1)return true;   //最后一次是3/3=1
-        if (n % 3 != 0)return false;   //不是3倍数
+    bool isPowerOfThree(int n) {  //使用递归，3的n次幂特点：能一直被3整除
+        if (n == 0)return false;  //上一次返回false，未被3整除
+        if (n == 1)return true;   //上一次返回true，被3整除
+        if (n % 3 != 0)return false;   //不是3倍数,返回false(0)
         return isPowerOfThree(n / 3);
     }
 };
