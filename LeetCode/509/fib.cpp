a0793f6ed27cbb14c99841c93d8971becc771a84
BETA_TOT🌸🌸β <lixinzhouli123@gmail.com>
2025-04-08 21:08:09 +0800

    Create fib.cpp

---
+++ LeetCode/509/fib.cpp
@@ -0,0 +1,24 @@
+#include<stdlib.h>
+#include<iostream>
+
+using namespace std;
+class Solution {
+public:
+	int fib(int n) {            //复杂度O(n) 
+	if (n == 0)return 0;
+	if (n == 1)return 1;
+	int pre = 0, cur = 1;       //F(0),F(1)
+	for (int i = 2; i <= n; i++) {
+		int next = pre + cur;  //斐波那契数
+		pre = cur;      //回溯
+		cur = next;
+	}
+	return cur;
+	}
+
+	int fib2(int n) {          //复杂度O(2^n) ,使用了斐波那契数列的原概念: F(n)=F(n-1)+F(n-2);F(0)=0,F(1)=1;
+		if (n == 0)return 0;
+		if (n == 1)return 1;
+		return fib2(n - 1) + fib(n - 2);
+	}
+};
